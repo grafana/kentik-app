af83d9ca9f44cd91082f37b0974480999e3099bf
Anthony Woods <awoods@raintank.io>
2016-02-29 19:16:26 +0800

    remove verbose logging

--- src/panel/device-list/module.js
+++ src/panel/device-list/module.js
@@ -20,21 +20,18 @@ class DeviceListCtrl extends PanelCtrl {
   getDevices() {
     var self = this;
     this.backendSrv.get("/api/plugin-proxy/kentik-app/api/device/list").then(function(resp) {
-      console.log(resp);
       self.devices = resp.device;
       self.pageReady = true;
     });
   }
 
   refresh() {
-    console.log("Kentik Device List: refresh called.");
     this.getDevices()
   }
 
   gotoDeviceDetail(device) {
     this.$location.url("/plugins/kentik-app/pags/device-detail?device="+device.id);
   }
-
 }
 
 DeviceListCtrl.templateUrl = 'panel/device-list/module.html';
