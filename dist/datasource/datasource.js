1badf91da57014e90db11f23f01782d25d349e71
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-04-06 20:50:31 -0400

    added device and metric discovery

--- dist/datasource/datasource.js
+++ dist/datasource/datasource.js
@@ -31,12 +31,12 @@ System.register([], function (_export, _context) {
       }();
 
       _export('KentikDatasource', KentikDatasource = function () {
-        function KentikDatasource(instanceSettings, backendSrv, $q) {
+        function KentikDatasource(instanceSettings, backendSrv, templateSrv) {
           _classCallCheck(this, KentikDatasource);
 
           this.instanceSettings = instanceSettings;
           this.backendSrv = backendSrv;
-          this.$q = $q;
+          this.templateSrv = templateSrv;
         }
 
         _createClass(KentikDatasource, [{
@@ -49,20 +49,24 @@ System.register([], function (_export, _context) {
         }, {
           key: 'query',
           value: function query(options) {
-            // var query = options.targets[0].target;
-            // query = query.replace('$timeFilter', this.getTimeFilter(options.range));
+            if (!options.targets || options.targets.length === 0) {
+              return Promise.resolve({ data: [] });
+            }
+
+            var target = options.targets[0];
+
             var query = {
               version: "2.01",
               query: {
-                device_name: 'nntp2a',
+                device_name: this.templateSrv.replace(target.device),
                 time_type: 'relative', // or fixed
                 lookback_seconds: 3600,
                 starting_time: null,
                 ending_time: null,
-                metric: "Traffic",
-                fast_data: "Auto", // or Fast or Full
-                device_type: 'router' },
-              // or host,
+                metric: this.templateSrv.replace(target.metric),
+                fast_data: "Auto" },
+              // or Fast or Full
+              // device_type: 'router', // or host,
               filterSettings: {
                 connector: 'All',
                 filterString: '',
@@ -75,13 +79,13 @@ System.register([], function (_export, _context) {
               method: 'POST',
               url: 'api/plugin-proxy/kentik-app/api/v4/dataExplorer/timeSeriesData',
               data: query
-            }).then(this.processResponse.bind(this));
+            }).then(this.processResponse.bind(this, query));
           }
         }, {
           key: 'processResponse',
-          value: function processResponse(data) {
+          value: function processResponse(query, data) {
             if (!data.data) {
-              return this.$q.reject({ message: 'no kentik data' });
+              return Promise.reject({ message: 'no kentik data' });
             }
 
             var rows = data.data;
@@ -89,8 +93,9 @@ System.register([], function (_export, _context) {
               return [];
             }
 
+            console.log('kentik response', rows);
             var seriesList = [];
-            var series = { target: 'traffic', datapoints: [] };
+            var series = { target: query.query.metric, datapoints: [] };
 
             for (var i = 0; i < rows.length; i++) {
               var row = rows[i];
@@ -101,6 +106,33 @@ System.register([], function (_export, _context) {
 
             seriesList.push(series);
             return { data: seriesList };
+          }
+        }, {
+          key: 'getMetricList',
+          value: function getMetricList() {
+            return Promise.resolve(["Traffic", "Geography_src", "src_geo_region", "src_geo_city", "AS_src", "InterfaceID_src", "Port_src", "VLAN_src", "IP_src", "src_route_prefix_len", "src_route_length", "src_bgp_community", "src_bgp_aspath", "src_nexthop_ip", "src_nexthop_asn", "src_second_asn", "src_third_asn", "src_proto_port", "TopFlow", "Proto", "InterfaceTopTalkers", "PortPortTalkers", "RegionTopTalkers", "TopFlowsIP", "ASTopTalkers", "TOS", "Geography_dst", "dst_geo_region", "dst_geo_city", "AS_dst", "InterfaceID_dst", "Port_dst", "VLAN_dst", "IP_dst", "dst_route_prefix_len", "dst_route_length", "dst_bgp_community", "dst_bgp_aspath", "dst_nexthop_ip", "dst_nexthop_asn", "dst_second_asn", "dst_third_asn", "dst_proto_port"].map(function (value) {
+              return { text: value, value: value };
+            }));
+          }
+        }, {
+          key: 'metricFindQuery',
+          value: function metricFindQuery(query) {
+            if (query === 'metrics()') {
+              return this.getMetricList();
+            }
+
+            return this.backendSrv.datasourceRequest({
+              method: 'GET',
+              url: 'api/plugin-proxy/kentik-app/api/v1/device/list'
+            }).then(function (res) {
+              if (!res.data || !res.data.device) {
+                return [];
+              }
+
+              return res.data.device.map(function (device) {
+                return { text: device.device_name, value: device.device_name };
+              });
+            });
           }
         }]);
 
