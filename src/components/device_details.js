258aa2ff94fd8aa35847769c1f1d8603fcc86648
woodsaj <anthony@monkey.id.au>
2016-04-07 20:07:41 +0800

    add basic details page.
    just dumps the response from the API in json format

--- src/components/device_details.js
+++ src/components/device_details.js
@@ -1,5 +1,19 @@
 class DeviceDetailsCtrl {
-
+	/** @ngInject */
+  constructor($scope, $injector, $location, backendSrv) {
+    this.backendSrv = backendSrv;
+    this.$location = $location;
+    this.device = {};
+    this.pageReady = false;
+    this.getDevice($location.search().device);
+  }
+  getDevice(deviceId) {
+    var self = this;
+    this.backendSrv.get("/api/plugin-proxy/kentik-app/api/device/"+deviceId).then(function(resp) {
+      self.device = resp.device[0];
+      self.pageReady = true;
+    });
+  }
 }
 
 DeviceDetailsCtrl.templateUrl = 'components/device_details.html';
