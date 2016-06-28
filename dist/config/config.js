cea9b320c4aa8056c4dbf1defcd72647d82cd6f2
Matt <matt@raintank.io>
2016-06-28 10:42:16 -0700

    Removed errant kentik stuff

--- dist/config/config.js
+++ dist/config/config.js
@@ -1,9 +1,9 @@
 'use strict';
 
-System.register(['./config.html!text', 'lodash'], function (_export, _context) {
+System.register(['./config.html!text'], function (_export, _context) {
   "use strict";
 
-  var configTemplate, _, _createClass, KentikConfigCtrl;
+  var configTemplate, PoolConfigCtrl;
 
   function _classCallCheck(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
@@ -14,133 +14,15 @@ System.register(['./config.html!text', 'lodash'], function (_export, _context) {
   return {
     setters: [function (_configHtmlText) {
       configTemplate = _configHtmlText.default;
-    }, function (_lodash) {
-      _ = _lodash.default;
     }],
     execute: function () {
-      _createClass = function () {
-        function defineProperties(target, props) {
-          for (var i = 0; i < props.length; i++) {
-            var descriptor = props[i];
-            descriptor.enumerable = descriptor.enumerable || false;
-            descriptor.configurable = true;
-            if ("value" in descriptor) descriptor.writable = true;
-            Object.defineProperty(target, descriptor.key, descriptor);
-          }
-        }
+      _export('ConfigCtrl', PoolConfigCtrl = function PoolConfigCtrl() {
+        _classCallCheck(this, PoolConfigCtrl);
+      });
 
-        return function (Constructor, protoProps, staticProps) {
-          if (protoProps) defineProperties(Constructor.prototype, protoProps);
-          if (staticProps) defineProperties(Constructor, staticProps);
-          return Constructor;
-        };
-      }();
+      PoolConfigCtrl.template = configTemplate;
 
-      _export('ConfigCtrl', KentikConfigCtrl = function () {
-        function KentikConfigCtrl($scope, $injector, backendSrv) {
-          _classCallCheck(this, KentikConfigCtrl);
-
-          this.backendSrv = backendSrv;
-
-          this.appEditCtrl.setPreUpdateHook(this.preUpdate.bind(this));
-          this.appEditCtrl.setPostUpdateHook(this.postUpdate.bind(this));
-
-          if (!this.appModel.jsonData) {
-            this.appModel.jsonData = {};
-          }
-          if (!this.appModel.secureJsonData) {
-            this.appModel.secureJsonData = {};
-          }
-          this.apiValidated = false;
-          this.apiError = false;
-          if (this.appModel.enabled && this.appModel.jsonData.tokenSet) {
-            this.validateApiConnection();
-          }
-        }
-
-        _createClass(KentikConfigCtrl, [{
-          key: 'preUpdate',
-          value: function preUpdate() {
-            if (this.appModel.secureJsonData.token) {
-              this.appModel.jsonData.tokenSet = true;
-            }
-
-            return this.initDatasource();
-          }
-        }, {
-          key: 'postUpdate',
-          value: function postUpdate() {
-            if (!this.appModel.enabled) {
-              return Promise.resolve();
-            }
-            var self = this;
-            return this.validateApiConnection().then(function () {
-              return self.appEditCtrl.importDashboards().then(function () {
-                return {
-                  url: "dashboard/db/kentik-home",
-                  message: "Kentik Connect Pro app installed!"
-                };
-              });
-            });
-          }
-        }, {
-          key: 'validateApiConnection',
-          value: function validateApiConnection() {
-            var self = this;
-            var promise = this.backendSrv.get('/api/plugin-proxy/kentik-app/api/v1/user/list');
-            promise.then(function () {
-              self.apiValidated = true;
-            }, function () {
-              self.apiValidated = false;
-              self.apiError = true;
-            });
-            return promise;
-          }
-        }, {
-          key: 'reset',
-          value: function reset() {
-            this.appModel.jsonData.email = '';
-            this.appModel.jsonData.tokenSet = false;
-            this.appModel.secureJsonData = {};
-            this.apiValidated = false;
-          }
-        }, {
-          key: 'initDatasource',
-          value: function initDatasource() {
-            var self = this;
-            //check for existing datasource.
-            return self.backendSrv.get('/api/datasources').then(function (results) {
-              var foundKentic = false;
-              _.forEach(results, function (ds) {
-                if (foundKentic) {
-                  return;
-                }
-                if (ds.name === "kentik") {
-                  foundKentic = true;
-                }
-              });
-              var promises = [];
-              if (!foundKentic) {
-                // create datasource.
-                var kentik = {
-                  name: 'kentik',
-                  type: 'kentik-ds',
-                  access: 'direct',
-                  jsonData: {}
-                };
-                promises.push(self.backendSrv.post('/api/datasources', kentik));
-              }
-              return Promise.all(promises);
-            });
-          }
-        }]);
-
-        return KentikConfigCtrl;
-      }());
-
-      KentikConfigCtrl.template = configTemplate;
-
-      _export('ConfigCtrl', KentikConfigCtrl);
+      _export('ConfigCtrl', PoolConfigCtrl);
     }
   };
 });
--- src/config/config.js
+++ src/config/config.js
@@ -1,100 +1,13 @@
 import configTemplate from './config.html!text';
 
-import _ from 'lodash' ;
-
-class KentikConfigCtrl {
-  constructor($scope, $injector, backendSrv) {
-    this.backendSrv = backendSrv;
-
-    this.appEditCtrl.setPreUpdateHook(this.preUpdate.bind(this));
-    this.appEditCtrl.setPostUpdateHook(this.postUpdate.bind(this));
-
-    if (!this.appModel.jsonData) {
-      this.appModel.jsonData = {};
-    }
-    if (!this.appModel.secureJsonData) {
-      this.appModel.secureJsonData = {};
-    }
-    this.apiValidated = false;
-    this.apiError = false;
-    if (this.appModel.enabled && this.appModel.jsonData.tokenSet) {
-      this.validateApiConnection();
-    }
-  }
-
-  preUpdate() {
-    if (this.appModel.secureJsonData.token)  {
-      this.appModel.jsonData.tokenSet = true;
-    }
-
-    return this.initDatasource();
-  }
-
-  postUpdate() {
-    if (!this.appModel.enabled) {
-      return Promise.resolve();
-    }
-    var self = this;
-    return this.validateApiConnection().then(() => {
-      return self.appEditCtrl.importDashboards().then(() => {
-        return {
-          url: "dashboard/db/kentik-home",
-          message: "Kentik Connect Pro app installed!"
-        };
-      });
-    });
-  }
-
-  // make sure that we can hit the Kentik API.
-  validateApiConnection() {
-    var self = this;
-    var promise = this.backendSrv.get('/api/plugin-proxy/kentik-app/api/v1/user/list');
-    promise.then(() => {
-      self.apiValidated = true;
-    }, () => {
-      self.apiValidated = false;
-      self.apiError = true;
-    });
-    return promise;
-  }
-
-  reset() {
-    this.appModel.jsonData.email = '';
-    this.appModel.jsonData.tokenSet = false;
-    this.appModel.secureJsonData = {};
-    this.apiValidated = false;
-  }
-
-  initDatasource() {
-    var self = this;
-    //check for existing datasource.
-    return self.backendSrv.get('/api/datasources').then(function(results) {
-      var foundKentic = false;
-      _.forEach(results, function(ds) {
-        if (foundKentic) { return; }
-        if (ds.name === "kentik") {
-          foundKentic = true;
-        }
-      });
-      var promises = [];
-      if (!foundKentic) {
-        // create datasource.
-        var kentik = {
-          name: 'kentik',
-          type: 'kentik-ds',
-          access: 'direct',
-          jsonData: {}
-        };
-        promises.push(self.backendSrv.post('/api/datasources', kentik));
-      }
-      return Promise.all(promises);
-    });
+class PoolConfigCtrl {
+  constructor() {
   }
 }
 
-KentikConfigCtrl.template = configTemplate;
+PoolConfigCtrl.template = configTemplate;
 
 export {
-  KentikConfigCtrl as ConfigCtrl
+  PoolConfigCtrl as ConfigCtrl
 };
 
