138f0013065686659a9583445f2ea95e14eca989
Matt <matt@raintank.io>
2016-06-28 11:00:30 -0700

    Removed dashbard directory, wired up the last 2 buttons

--- src/datasource/module.js
+++
@@ -1,9 +0,0 @@
-import {KentikDatasource} from './datasource';
-import {ConfigCtrl} from './config';
-import {KentikQueryCtrl} from './query_editor';
-
-export {
-  KentikDatasource as Datasource,
-  ConfigCtrl as ConfigCtrl,
-  KentikQueryCtrl as QueryCtrl
-};
