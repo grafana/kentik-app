da775e38459e810ca790bcf009bc0f6062a0bdcb
Daniel Lee <dan.limerick@gmail.com>
2018-10-22 16:29:21 +0200

    v1.3.0 release

---
+++ dist/components/add_device.ts
@@ -0,0 +1,52 @@
+import * as _ from 'lodash';
+import angular from 'angular';
+
+const defaults = {
+  device_name: '',
+  device_type: 'router',
+  device_description: '',
+  device_flow_type: 'sflow',
+  device_sample_rate: 5,
+  sending_ips: '',
+  minimize_snmp: false,
+  device_bgp_type: 'none',
+  device_snmp_ip: '',
+  device_snmp_community: '',
+};
+
+export class AddDeviceCtrl {
+  static templateUrl: string;
+  device: any;
+  sendingIps: any[];
+
+  /** @ngInject */
+  constructor($scope, $injector, public $location: any, public backendSrv: any, public alertSrv: any) {
+    this.device = angular.copy(defaults);
+    this.sendingIps = [{ ip: '' }];
+  }
+
+  addIP() {
+    this.sendingIps.push({ ip: '' });
+  }
+
+  removeIP(index) {
+    this.sendingIps.splice(index, 1);
+  }
+
+  addDevice() {
+    const ips = [];
+    _.forEach(this.sendingIps, ip => {
+      ips.push(ip.ip);
+    });
+    this.device.sending_ips = ips.join();
+    this.backendSrv.post('/api/plugin-proxy/kentik-app/api/v5/device', this.device).then(resp => {
+      if ('err' in resp) {
+        this.alertSrv.set('Device Add failed.', resp.err, 'error');
+      } else {
+        this.$location.url('/plugins/kentik-app/page/device-list');
+      }
+    });
+  }
+}
+
+AddDeviceCtrl.templateUrl = 'components/add_device.html';
---
+++ dist/components/device_list.ts
@@ -0,0 +1,34 @@
+class DeviceListCtrl {
+  static templateUrl: string;
+  devices: any[];
+  pageReady: boolean;
+
+  /** @ngInject */
+  constructor($scope, $injector, public $location: any, public backendSrv: any) {
+    this.devices = [];
+    this.pageReady = false;
+    this.getDevices();
+  }
+  getDevices() {
+    this.backendSrv.get('/api/plugin-proxy/kentik-app/api/v5/devices').then(resp => {
+      this.devices = resp.devices;
+      this.pageReady = true;
+    });
+  }
+
+  refresh() {
+    this.getDevices();
+  }
+
+  gotoDashboard(device) {
+    this.$location.path('/dashboard/db/kentik-top-talkers').search({ 'var-device': device.device_name });
+  }
+
+  gotoDeviceDetail(device) {
+    this.$location.url('/plugins/kentik-app/page/device-details?device=' + device.id);
+  }
+}
+
+DeviceListCtrl.templateUrl = 'components/device_list.html';
+
+export { DeviceListCtrl };
