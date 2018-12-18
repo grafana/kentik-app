388ab518bcc90fb82712d3a82b51154418a00290
Brian Gann <bkgann@gmail.com>
2018-12-18 16:30:28 -0600

    release 1.3.2

---
+++ dist/components/device_list.ts
@@ -0,0 +1,43 @@
+import { KentikAPI, showAlert } from '../datasource/kentikAPI';
+
+class DeviceListCtrl {
+  static templateUrl: string;
+  devices: any[];
+  pageReady: boolean;
+  kentik: KentikAPI;
+
+  /** @ngInject */
+  constructor(private $scope, $injector, public $location: any, public backendSrv: any) {
+    this.devices = [];
+    this.pageReady = false;
+    this.kentik = new KentikAPI(this.backendSrv);
+    this.getDevices();
+  }
+
+  async getDevices() {
+    try {
+      this.devices = await this.kentik.getDevices();
+      this.pageReady = true;
+
+      this.$scope.$apply();
+    } catch (e) {
+      showAlert(e);
+    }
+  }
+
+  refresh() {
+    this.getDevices();
+  }
+
+  gotoDashboard(device) {
+    this.$location.path('/dashboard/db/kentik-top-talkers').search({ 'var-device': device.device_name });
+  }
+
+  gotoDeviceDetail(device) {
+    this.$location.url('/plugins/kentik-app/page/device-details?device=' + device.id);
+  }
+}
+
+DeviceListCtrl.templateUrl = 'components/device_list.html';
+
+export { DeviceListCtrl };
