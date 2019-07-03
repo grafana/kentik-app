936dae468583244206dc6b941dc182d3b21002ab
ryan <ryantxu@gmail.com>
2019-07-02 21:30:12 -0700

    remove webpack configs

--- build/webpack.prod.conf.js
+++
@@ -1,7 +0,0 @@
-const baseWebpackConfig = require('./webpack.base.conf');
-
-var conf = baseWebpackConfig;
-conf.mode = 'production';
-conf.devtool = 'inline-source-map';
-
-module.exports = baseWebpackConfig;
