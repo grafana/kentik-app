5eea3b05203b2ab710abd357a7cc0380cd9e58dc
ryan <ryantxu@gmail.com>
2019-07-03 11:14:50 -0700

    move sass to styles

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,3 @@
-import './sass/kentik.dark.scss';
-import './sass/kentik.light.scss';
-
 import { ConfigCtrl } from './config/config';
 import { DeviceListCtrl } from './components/device_list';
 import { DeviceDetailsCtrl } from './components/device_details';
@@ -8,8 +5,8 @@ import { AddDeviceCtrl } from './components/add_device';
 import { loadPluginCss } from 'grafana/app/plugins/sdk';
 
 loadPluginCss({
-  dark: 'plugins/kentik-app/css/kentik.dark.css',
-  light: 'plugins/kentik-app/css/kentik.light.css',
+  dark: 'plugins/kentik-app/styles/dark.css',
+  light: 'plugins/kentik-app/styles/light.css',
 });
 
 export { DeviceListCtrl, DeviceDetailsCtrl, AddDeviceCtrl, ConfigCtrl };
